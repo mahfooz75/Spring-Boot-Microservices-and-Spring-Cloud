6ac1ffb3a5696f52208ed27e916a357b4bd4ca06
mahfoo75 <mahfooz.lakhani@gmail.com>
2022-03-18 07:06:54 +0530

    Added Spring Security basic

--- user-service/src/main/java/com/selftech/microservices/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/selftech/microservices/userservice/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.selftech.microservices.userservice.entity.UserEntity;
@@ -15,21 +16,23 @@ import com.selftech.microservices.userservice.shared.Util;
 public class UserServiceImpl implements UserService {
 
 	UserRepository userRepository;
-	@Autowired
-	public UserServiceImpl(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
+	BCryptPasswordEncoder bCryptPasswordEncoder;
 
+	@Autowired
+	public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.userRepository = userRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
 
 	@Override
 	public UserDto createUser(UserDto userDetails) {
-		
+
 		userDetails.setUserId(UUID.randomUUID().toString());
-		ModelMapper mapper=Util.getModelMapper();
+		userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
+		ModelMapper mapper = Util.getModelMapper();
 		UserEntity userEntity = mapper.map(userDetails, UserEntity.class);
-		userEntity.setEncryptedPassword("test");
 		UserEntity savedEntity = userRepository.save(userEntity);
-		
+
 		UserDto userDto = mapper.map(savedEntity, UserDto.class);
 		return userDto;
 	}
